9cfb75d6a9cd14637026813dee326e23e94c9a77
yeonnex <yeonnex@gmail.com>
2022-12-08 00:44:04 +0900

    order

---
+++ src/main/java/com/myshop/Money.java
@@ -0,0 +1,8 @@
+package com.myshop;
+
+public class Money {
+    private int value;
+    Money(int value) {
+        this.value = value;
+    }
+}
--- src/main/java/com/myshop/Order.java
+++ src/main/java/com/myshop/Order.java
@@ -1,12 +1,69 @@
 package com.myshop;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+import java.util.List;
+
 public class Order {
-    private OrderStatus state;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+    private List<OrderLine> orderLines;
+    private Money totalAmounts;
     private ShippingInfo shippingInfo;
 
+    public Order(List<OrderLine> orderLines, ShippingInfo shippingInfo) {
+        setOrderLines(orderLines);
+        setShippingInfo(shippingInfo);
+    }
+
+    /**
+     * 배송 정보를 바꾼다
+     * @param newShippingInfo
+     */
     public void changeShippingInfo(ShippingInfo newShippingInfo) {
-        if (!state.isShippingChangeable()) {
-            throw new IllegalStateException("can't change shipping info in " + state);
+        verifyNotYetShipped();
+        setShippingInfo(shippingInfo);
+    }
+
+    /**
+     * 주문을 취소한다
+     */
+    public void cancel() {
+        verifyNotYetShipped();
+        this.status = OrderStatus.CANCELED;
+    }
+
+    private void setShippingInfo(ShippingInfo shippingInfo) {
+        if (shippingInfo == null){
+            throw new IllegalArgumentException("no ShippingInfo");
+        }
+        this.shippingInfo = shippingInfo;
+    }
+
+    private void setOrderLines(List<OrderLine> orderLines) {
+        verifyAtLeastOneOrMoreOrderLines(orderLines);
+        this.orderLines = orderLines;
+        calculateTotalAmounts();
+    }
+
+    private void calculateTotalAmounts() {
+        int sum = orderLines.stream()
+                .mapToInt(x -> x.getAmounts())
+                .sum();
+        this.totalAmounts = new Money(sum);
+    }
+
+    private void verifyAtLeastOneOrMoreOrderLines(List<OrderLine> orderLines) {
+        if (orderLines == null || orderLines.isEmpty()) {
+            throw new IllegalArgumentException("no OrderLine");
+        }
+    }
+
+    private void verifyNotYetShipped() {
+        if (! status.isShippingChangeable()) {
+            throw new IllegalStateException("already shipped");
         }
     }
 }
